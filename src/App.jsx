6ed22939eb82e8f4b679a67360bf214c801338d1
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-19 17:04:21 -0500

    Create Blog page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Footer } from "components/Footer";
 import { Navbar } from "components/Navbar";
 
+import { Blog } from "pages/Blog";
 import { Create } from "pages/Create";
 import { Home } from "pages/Home";
 
@@ -17,6 +18,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/create" element={<Create />} />
+                <Route path="/blog/:id" element={<Blog />} />
             </Routes>
             <Footer />
         </Router>
---
+++ src/pages/Blog/index.jsx
@@ -0,0 +1,9 @@
+const Blog = () => {
+    return (
+        <div className="blog-wrapper">
+            <h2>Blog Works!</h2>
+        </div>
+    );
+};
+
+export { Blog };
