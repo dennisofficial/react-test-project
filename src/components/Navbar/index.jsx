6307cec4fc0aef7feb07240b50b16ce125ed8f48
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-19 16:37:17 -0500

    Seperate styles to different files

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
+
+import { Create } from "pages/Create";
+import { Home } from "pages/Home";
+
+import "./_account.scss";
+import "./_brand.scss";
+import "./_search.scss";
 import "./style.scss";
 
 const Navbar = () => {
     return (
         <div className="navbar-wrapper">
-            <div className="navbar container">
+            <div className="navbar-header container">
                 <div className="brand">
                     <i className="bi bi-amd"></i>
                     <span>AMD</span>
