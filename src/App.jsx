5c715fedbd705be70182ea61cd2518853de7ef29
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-19 12:24:19 -0500

    Add Navbar & Footer to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { Home } from "pages/home";
+import { Footer } from "components/Footer";
+import { Navbar } from "components/Navbar";
+import { Home } from "pages/Home";
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Navbar />
             <Switch>
                 <Route path="/">
                     <Home />
                 </Route>
             </Switch>
+            <Footer />
         </BrowserRouter>
     );
 };
