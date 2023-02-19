d4a8f1ca8ca72617039e4de00928d9a117079e24
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-19 14:31:17 -0500

    Add Create Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
+import { Create } from "pages/Create";
 import { Home } from "pages/Home";
 
 const App = () => {
@@ -7,7 +8,7 @@ const App = () => {
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/" element={<Home />} />
+                <Route path="/create" element={<Create />} />
             </Routes>
         </Router>
     );
---
+++ src/pages/Create/index.jsx
@@ -0,0 +1,9 @@
+const Create = () => {
+    return (
+        <div className="create-wrapper">
+            <h1>Create works!</h1>
+        </div>
+    );
+};
+
+export { Create };
