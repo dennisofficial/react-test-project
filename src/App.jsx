6dacf161b742d23494a426c232b8321a0a7c1c23
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-19 16:14:20 -0500

    Added Navbar and Footer to App root

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
+import { Footer } from "components/Footer";
+import { Navbar } from "components/Navbar";
+
 import { Create } from "pages/Create";
 import { Home } from "pages/Home";
 
 const App = () => {
     return (
         <Router>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/create" element={<Create />} />
             </Routes>
+            <Footer />
         </Router>
     );
 };
