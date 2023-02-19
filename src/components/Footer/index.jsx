831bd37cccd07bb87defaa790e87325cdec66070
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-19 16:15:42 -0500

    Add content, and styles to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,16 @@
+import "./style.scss";
+
 const Footer = () => {
-    return <div className="footer-wrapper"></div>;
+    return (
+        <div className="footer-wrapper">
+            <div className="footer container">
+                <div className="copyright">
+                    <span>Dennis Lysenko, {new Date().getFullYear()}</span>
+                    <i className="bi bi-c-circle"></i>
+                </div>
+            </div>
+        </div>
+    );
 };
 
-export default Footer;
+export { Footer };
