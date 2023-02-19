763bab0e928911d13b70b6b8a42a14fe7c18d8fc
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-19 16:15:56 -0500

    Added content and styles to Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,29 @@
+import "./style.scss";
+
 const Navbar = () => {
-    return <div className="navbar-wrapper"></div>;
+    return (
+        <div className="navbar-wrapper">
+            <div className="navbar container">
+                <div className="brand">
+                    <i className="bi bi-amd"></i>
+                    <span>AMD</span>
+                </div>
+                <div className="search">
+                    <i className="bi bi-search"></i>
+                    <input
+                        type="text"
+                        name="query"
+                        id="search-bar"
+                        placeholder="search..."
+                    />
+                </div>
+                <div className="account">
+                    <span>Account</span>
+                    <i className="bi bi-person-circle"></i>
+                </div>
+            </div>
+        </div>
+    );
 };
 
-export default Navbar;
+export { Navbar };
