46b3ba6d2257b26ef7949df5efbcd19c2335999b
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-21 22:27:17 -0500

    Move BlogList to `components`

---
+++ client/src/components/BlogList/index.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import "./styles.scss";
+
+const BlogList = ({ data }) => {
+    return (
+        <div className="bloglist-wrapper">
+            <Link to={`blogs/${data.id}`}>
+                <div className="blog-preview" key={data.id}>
+                    <div className="header">
+                        <h2 className="title">{data.title}</h2>
+                        <span className="author">Author: {data.author}</span>
+                    </div>
+                    <p className="content">{data.content}</p>
+                </div>
+            </Link>
+        </div>
+    );
+};
+
+export { BlogList };
