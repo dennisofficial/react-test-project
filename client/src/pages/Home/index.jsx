ea972daa59abd9637c405c7ccd256f489d54a97d
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-21 22:28:42 -0500

    Moved data fetching from blog-list to home page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,12 +1,25 @@
-import { BlogList } from "./components/BlogList";
+import { BlogList } from "components/BlogList";
+import { useFetch } from "hooks/useFetch";
 
+import { Link } from "react-router-dom";
 import "./styles.scss";
 
 const Home = () => {
+    const { data, isLoading, getError } = useFetch(
+        "http://localhost:8000/blogs"
+    );
+
     return (
         <div className="home-wrapper">
             <div className="container">
-                <BlogList />
+                {data &&
+                    data.map((blog) => {
+                        <Link to="">
+                            <BlogList data={data} />
+                        </Link>;
+                    })}
+                {isLoading && <p>Loading</p>}
+                {getError && <p>{getError}</p>}
             </div>
         </div>
     );
