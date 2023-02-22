4acea270995f5be07aae6cc091931920e76eff08
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-21 22:36:12 -0500

    Removed `Link` from BlogItem

--- client/src/components/BlogItem/index.jsx
+++ client/src/components/BlogItem/index.jsx
@@ -1,18 +1,15 @@
-import { Link } from "react-router-dom";
 import "./styles.scss";
 
 const BlogItem = ({ data }) => {
     return (
         <div className="bloglist-wrapper">
-            <Link to={`blogs/${data.id}`}>
-                <div className="blog-preview" key={data.id}>
-                    <div className="header">
-                        <h2 className="title">{data.title}</h2>
-                        <span className="author">Author: {data.author}</span>
-                    </div>
-                    <p className="content">{data.content}</p>
+            <div className="blog-preview" key={data.id}>
+                <div className="header">
+                    <h2 className="title">{data.title}</h2>
+                    <span className="author">Author: {data.author}</span>
                 </div>
-            </Link>
+                <p className="content">{data.content}</p>
+            </div>
         </div>
     );
 };
