fb1490b26b5cfb3e2d6a4cf017e9492e62e1ad8f
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-21 22:44:26 -0500

    Remove uneeded div

--- client/src/components/BlogItem/index.jsx
+++ client/src/components/BlogItem/index.jsx
@@ -1,13 +1,11 @@
 const BlogItem = ({ data }) => {
     return (
-        <div className="bloglist-wrapper">
-            <div className="blog-preview" key={data.id}>
-                <div className="header">
-                    <h2 className="title">{data.title}</h2>
-                    <span className="author">Author: {data.author}</span>
-                </div>
-                <p className="content">{data.content}</p>
+        <div className="blog-preview" key={data.id}>
+            <div className="header">
+                <h2 className="title">{data.title}</h2>
+                <span className="author">Author: {data.author}</span>
             </div>
+            <p className="content">{data.content}</p>
         </div>
     );
 };
