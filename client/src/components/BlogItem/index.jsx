f4d53de61971fee03b0f6a36dd98b5a94b87f78a
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-21 22:45:58 -0500

    Renamed class to blog-item

--- client/src/components/BlogItem/index.jsx
+++ client/src/components/BlogItem/index.jsx
@@ -1,6 +1,6 @@
 const BlogItem = ({ data }) => {
     return (
-        <div className="blog-preview" key={data.id}>
+        <div className="blog-item" key={data.id}>
             <div className="header">
                 <h2 className="title">{data.title}</h2>
                 <span className="author">Author: {data.author}</span>
