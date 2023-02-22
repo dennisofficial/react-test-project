2d3980b7f15b7c3baff174ba777739ed77a7d3a4
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-21 21:32:16 -0500

    Change BlogList style and content

--- client/src/pages/Home/components/BlogList/index.jsx
+++ client/src/pages/Home/components/BlogList/index.jsx
@@ -1,5 +1,6 @@
 import { useFetch } from "hooks/useFetch";
 
+import { Link } from "react-router-dom";
 import "./styles.scss";
 
 const BlogList = () => {
@@ -11,10 +12,17 @@ const BlogList = () => {
         <div className="bloglist-wrapper">
             {data &&
                 data.map((blog) => (
-                    <div className="blog-preview" key={blog.id}>
-                        <h2>{blog.title}</h2>
-                        <span>{blog.author}</span>
-                    </div>
+                    <Link to={`blogs/${blog.id}`}>
+                        <div className="blog-preview" key={blog.id}>
+                            <div className="header">
+                                <h2 className="title">{blog.title}</h2>
+                                <span className="author">
+                                    Author: {blog.author}
+                                </span>
+                            </div>
+                            <p className="content">{blog.content}</p>
+                        </div>
+                    </Link>
                 ))}
             {isLoading && <p>Loading</p>}
             {getError && <p>{getError}</p>}
