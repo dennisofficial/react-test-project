d15caaef25ed71e056fcf0c335235401b2fe03c7
Dennis Lysenko <dennislysenko@hotmail.com>
2023-02-23 18:28:58 -0500

    Init Express Server

--- server/App.js
+++ server/App.js
@@ -1,7 +1,20 @@
-express = require("express");
-const app = express();
-const port = 4000;
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
